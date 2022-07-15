f2e8b0f3a6bab1c3971c9b4079e8793dae97c1d9
BrandonBocheng <hubocheng1996@163.com>
2022-07-15 11:01:51 +0800

    function pointer practice part 2

--- FunctionPointer/ComplexFunctionPointer.cpp
+++ FunctionPointer/ComplexFunctionPointer.cpp
@@ -15,7 +15,21 @@ int main() {
     cout << (*pf1)(av, 3) << ": " << *(pf1)(av, 3) << endl;
     cout << pf2(av, 3) << ": " << *pf2(av, 3) << endl;
 
-    
+    // pa an array of pointers
+    const double *(*pa[3])(const double *, int) = {f1, f2, f3};
+
+    // auto doesn't work with list in
+    // but it does work for initializting to a single value
+    // pb a pointer to first element of pa
+
+    // auto pb = pa;
+    const double * (**pb)(const double *, int) = pa;
+    cout << "\nUsing an array of pointers to functions:\n";
+    cout << " Address Value\n";
+    for (int i = 0; i < 3; i++) {
+        cout << pa[i](av, 3) << ": " << *pa[i](av ,3) << endl;
+    }
+
     return 0;
 }
 
